e475270b28c3356d91aa4cd2131b7c1d813a2530
AnEagle <72992035+An-Eagle@users.noreply.github.com>
2025-07-25 14:53:02 +0200

    Temporarily disable virtualisation support

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,18 +10,18 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y virt-manager gdm niri 
-#install Astal dependencies
+dnf5 install -y gdm niri 
+#install AGS and dependencies
 dnf5 install -y dunst grim slurp gtk4 gtk4-layer-shell gjs NetworkManager NetworkManager-libnm-devel bluez wireplumber wireplumber-devel pipewire libnotify upower-devel libappindicator-gtk3-devel 
-
-dnf5 -y copr enable ublue-os/staging
-dnf5 -y install bazaar fwupd
-dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr enable solopasha/hyprland
 dnf5 -y install aylurs-gtk-shell2 cliphist nwg-look
 dnf5 -y copr disable solopasha/hyprland
-
-
+#Install bazaar
+dnf5 -y copr enable ublue-os/staging
+dnf5 -y install bazaar fwupd
+dnf5 -y copr disable ublue-os/staging
+#Virtualisation setup
+#dnf5 install -y virt-manager
 
 # Use a COPR Example:
 #
